3964d70973fc42baaab3e57fcf8917edae9ef560
Elizaveta Gorgeeva <57723993+ligradee@users.noreply.github.com>
2021-07-04 21:44:35 +0300

    Add file with data to generate events

---
+++ info.py
@@ -0,0 +1,46 @@
+country_array = {'United States':['New York', 'Los Angeles', 'Chicago'], 
+				 'Canada':['Toronto', 'Quebec City', 'Vancouver', 'Calgary'],
+				 'Russia':['Moscow', 'Saint Petersburg', 'Novosibirsk', 'Yekaterinburg', 'Kazan'],
+				 'France':['Paris', 'Marseille', 'Lyon', 'Toulouse', 'Nice']}
+
+language_array = ['French', 'English', 'Russian']
+platform_array = {'phone':['IOS', 'Android'], 'computer':['Windows', 'Linux', 'MacOS']}
+employment_array = ['student','employee','unemployed','part-time']
+gender = ['male', 'female']
+# status == null, user still didn't create account
+status_array = ['studying','had a trial lesson','just registered', 'null']
+
+directions_array = ['design', 'literature','history', 'programming', 'business']
+event_type_arrays = {'click_create_account':[],
+					 'enter_info':[],
+					 'account_created':[],
+					 'account_verified':[],
+					 'login':[],
+					 'click_signup_lesson':[],
+					 'choose_direction':['direction'],
+					 'choose_time':['interval'],
+					 'record_confirmation':[],
+					 'click_start_lesson':['direction', 'interval'],
+					 'click_allow_camera':[],
+					 'click_allow_microphone':[],
+					 'click_technical_trouble':[],
+					 'rate_lesson':['grade', 'teacher_id'],
+					 'click_pay_lessons':[],
+					 'choose_count_lessons':['count','direction'],
+					 'apply_promo_code':['promocode','discount_cost'],
+					 'bank_card_data_entry':['payment_system'],
+					 'click_confirm_payment':['total_cost','direction'],
+					 'click_change_direction':[],
+					 'choose_direction':['new_direction','last_direction'],
+					 'click_transfer_lesson':[],
+					 'choose_time':['new_interval','last_interval','direction'],
+					 'click_change_teacher':['teacher_id'],
+					 'click_finish_studies':[]}
+
+promo_array = ['funnyschool', 'literature2021', 'hi_school', 'meow']
+payment_system_array = ['Visa', 'MasterCard']
+interval_array = ['00:00 - 01:00','01:00 - 02:00', '02:00 - 03:00', '03:00 - 04:00', '04:00 - 05:00',
+'06:00 - 07:00', '07:00 - 08:00', '08:00 - 09:00', '09:00 - 10:00', '10:00 - 11:00', '11:00 - 12:00',
+'12:00 - 13:00', '13:00 - 14:00', '14:00 - 15:00', '15:00 - 16:00', '16:00 - 17:00', '17:00 - 18:00', 
+'19:00 - 20:00', '21:00 - 22:00', '22:00 - 23:00']
+
